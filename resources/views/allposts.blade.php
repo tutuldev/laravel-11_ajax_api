e17eec7e2a13a9ca4b71ec736dc4130040e1dc85
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-18 09:01:25 +0600

    ajax login with jqury

---
+++ resources/views/allposts.blade.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-8 mt-5">
+            <div class="card">
+                <div class="card-header">
+                    <h3>All Post</h3>
+                </div>
+             <div class="card-body">
+                <table class="table table-bordered table-striped text-center">
+                    <tr>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+
+                    </tr>
+                    <tr>
+                        <td class="px-5"></td>
+                        <td class="px-5">This is First title</td>
+                        <td class="px-5">This is First Post Description</td>
+
+                            <td>
+
+
+                                <div class="d-flex gap-2">
+                                    <a href="" class="btn btn-primary btn-sm">View</a>
+                                    <a href="" class="btn btn-warning btn-sm">Update</a>
+                                    <a href="" class="btn btn-primary btn-sm">View</a>
+
+                                </div>
+                            </td>
+
+                      </tr>
+                </table>
+             </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
