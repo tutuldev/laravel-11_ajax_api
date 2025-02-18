fd7865afa3fce1feea4275d2a542eeed128fcab8
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-02-18 10:12:15 +0600

    show data by modal box

--- resources/views/allposts.blade.php
+++ resources/views/allposts.blade.php
@@ -54,7 +54,23 @@
     </div>
 </div>
 
-
+<!--singel post modal Modal -->
+<div class="modal fade" id="singelPostModel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="singelPostLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title fs-5" id="singelPostLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
@@ -114,8 +130,10 @@ fetch('/api/posts',{
 
                                     </p>
                                 </td>
-                                <td><button type="button" class="btn btn-sm btn-primary">View</button></td>
-                                <td><button type="button" class="btn btn-sm btn-success">Update</button></td>
+                                <td>
+                                    <button type="button" class="btn btn-sm btn-primary data-bs-post="${post.id}" data-bs-toggle="modal" data-bs-target="#singelPostModel">View</button>
+                                </td>
+                                <td><button type="button" class="btn btn-sm btn-success data-bs-post="${post.id}" data-bs-toggle="modal" data-bs-target="#updatePostModel">Update</button></td>
                                 <td><button class="btn btn-sm btn-danger">Delete</button></td>
                               </tr>`
                             });
